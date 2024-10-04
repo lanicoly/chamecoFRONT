1cbafa187e4467cd50646baad15e828d26203c3a
lais <laisnicmns@gmail.com>
2024-10-03 22:43:39 -0300

    feat: editar sala e desselecionar sala

--- src/app.tsx
+++ src/app.tsx
@@ -30,16 +30,27 @@ const [nome, setNome] = useState('');
 const [descricao, setDescricao] = useState('');
 
 const [isSalaModalOpen, setIsSalaModalOpen] = useState(false);
+const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 const [salaSelecionada, setSalaSelecionada] = useState<number | null>(null);
 
 function openSalaModal() {
-    setIsSalaModalOpen(true);
+  setIsSalaModalOpen(true);
 }
 
 function closeSalaModal() {
   setNome('');
   setDescricao('');
   setIsSalaModalOpen(false);
+}
+
+function openEditModal() {
+  if (salaSelecionada) {
+    setIsEditModalOpen(true);
+  }
+}
+
+function closeEditModal() {
+  setIsEditModalOpen(false);
 }
 
 function addSala(e: React.FormEvent) {
@@ -55,6 +66,8 @@ function addSala(e: React.FormEvent) {
   setDescricao('');
   closeSalaModal();
 }
+
+
 function removeSala(){
   if (salaSelecionada !== null) {
     setListaSalas(listaSalas.filter(sala => sala.id !== salaSelecionada));
@@ -62,9 +75,42 @@ function removeSala(){
   }
 };
 
+
+function editaSala(e: React.FormEvent) {
+  e.preventDefault();
+  if (salaSelecionada !== null) {
+    listaSalas.map(sala =>{
+      if (sala.id === salaSelecionada){
+        if (nome) {
+          sala.nome = nome
+        }
+        if (descricao) {
+          sala.descricao = descricao
+        }
+      }
+    })
+    setSalaSelecionada(null);
+  }
+  setNome('');
+  setDescricao('');
+  closeEditModal();
+};
+
+function statusSala(id: number) {
+  if (salaSelecionada !== null){
+    desselecionaSala()
+  } else {
+    selecionaSala(id)
+  }
+}
+
 function selecionaSala (id: number) {
   setSalaSelecionada(id);
 };
+
+function desselecionaSala() {
+  setSalaSelecionada(null)
+}
 
 function avancarPagina(){
   if (paginaAtual < totalPaginas) {
@@ -82,7 +128,7 @@ function voltarPagina(){
     <div className="flex items-center justify-center bg-tijolos h-screen bg-no-repeat bg-cover">
 
     {/* parte informativa tela salas */}
-    <div className="relative bg-white w-full max-w-[600px] rounded-3xl px-6 py-6 m-24">
+    <div className="relative bg-white w-full max-w-[800px] rounded-3xl px-6 py-6 m-24 h-[90%]">
 
       {/* cabeçalho tela salas */}
        <div className="flex w-full gap-2">
@@ -98,11 +144,34 @@ function voltarPagina(){
 
 
         {/* conteudo central tela salas */}
-       <div className="flex flex-col px-8 py-4 w-auto justify-center gap-3">
+       <div className="flex flex-col px-8 py-4 w-auto justify-center gap-2">
 
           {/* adicionar sala + pesquisa */}
           <div className="flex justify-center items-center min-w-[220px] flex-wrap gap-3 flex-1 mobile:justify-between">
 
+
+
+
+            {/* input de pesquisa */}
+            <div className="h-fit items-center w-full mobile:w-auto">
+              <div className="flex justify-between items-center px-2 py-1 border-solid border-[1px] border-slate-500 rounded-md ">
+                <input
+                  type="text"
+                  value={pesquisa}
+                  onChange={(e) => {
+                    setPesquisa(e.target.value);
+                    setIsSearching(e.target.value.trim().length > 0);
+                  }}
+                  placeholder="Pesquisar..."
+                  className="placeholder-sky-900 text-xs font-medium outline-none "
+                />
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="#64748b" className="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+              </svg>
+              </div>
+
+            </div>
+            {/* fim input de pesquisa */}
             <button onClick={openSalaModal} className="px-4 py-1.5 bg-[#18C64F] text-white font-medium flex gap-2 justify-center items-center hover:bg-[#56ab71] rounded-md w-full mobile:w-auto">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" className="bi bi-plus-circle" viewBox="0 0 16 16">
               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
@@ -171,48 +240,6 @@ function voltarPagina(){
 
             {/* Fim adicionando pop up de adicionar salas */}
 
-            {/* {isSalaModalOpen && (
-              <form onSubmit={addSala} className="flex gap-2">
-                <input
-                  type="text"
-                  placeholder="Nome da Sala"
-                  value={nome}
-                  onChange={(e) => setNome(e.target.value)}
-                  required
-                />
-                <input
-                  type="text"
-                  placeholder="Descrição da Sala"
-                  value={descricao}
-                  onChange={(e) => setDescricao(e.target.value)}
-                  required
-                />
-                <button type="submit" className="px-2 py-1 bg-blue-500 text-white rounded">Adicionar</button>
-                <button onClick={closeSalaModal} type="button" className="px-2 py-1 bg-red-500 text-white rounded">Cancelar</button>
-              </form>
-            )} */}
-
-            {/* input de pesquisa */}
-            <div className="h-fit items-center w-full mobile:w-auto">
-              <div className="flex justify-between items-center px-2 py-1 border-solid border-[1px] border-slate-500 rounded-md ">
-                <input
-                  type="text"
-                  value={pesquisa}
-                  onChange={(e) => {
-                    setPesquisa(e.target.value);
-                    setIsSearching(e.target.value.trim().length > 0);
-                  }}
-                  placeholder="Pesquisar..."
-                  className="placeholder-sky-900 text-xs font-medium outline-none "
-                />
-                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="#64748b" className="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-              </svg>
-              </div>
-
-            </div>
-            {/* fim input de pesquisa */}
-
           </div>
           {/* fim adicionar sala + pesquisa */}
 
@@ -221,13 +248,71 @@ function voltarPagina(){
           <div>
 
             {/* botões editar e excluir */}
-            <div className="flex gap-4 justify-end my-4">
-            <a href="#" className="flex gap-1 justify-start items-center font-medium text-sm text-[#646999] underline">
+            <div className="flex gap-4 justify-end my-2">
+            <button onClick={openEditModal} className="flex gap-1 justify-start items-center font-medium text-sm text-[#646999] underline">
             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="#646999" className="bi bi-pen" viewBox="0 0 16 16">
               <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
             </svg>
               Editar
-            </a>           
+            </button>
+            {/* Adicionando pop up de editar sala */}
+            {isEditModalOpen && (
+              <div className="fixed flex items-center justify-center inset-0 bg-black bg-opacity-50">
+                <form onSubmit={editaSala} className="container flex flex-col gap-2 w-full p-[10px] h-auto rounded-[15px] bg-white mx-5 max-w-[400px]">
+                  <div className="flex justify-center mx-auto w-full max-w-[90%]">
+                    <p className="text-[#192160] text-center text-[20px] font-semibold  ml-[10px] w-[85%] ">
+                      EDITAR SALA
+                    </p>
+                    <button
+                      onClick={closeEditModal}
+                      type="button"
+                      className="px-2 py-1 rounded w-[5px] flex-shrink-0 "
+                    >
+                      <X className=" mb-[5px] text-[#192160]" />
+                    </button>
+                  </div>
+
+                  <div className="justify-center items-center ml-[40px] mr-8">
+                    <p className="text-[#192160] text-sm font-medium mb-1">
+                      Digite o novo nome da sala
+                    </p>
+
+                    
+                    <input
+                      className="w-full p-2 rounded-[10px] border border-[#646999] focus:outline-none text-[#777DAA] text-xs font-medium "
+                      type="text"
+                      placeholder="Sala"
+                      value={nome}
+                      onChange={(e) => setNome(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="justify-center items-center ml-[40px] mr-8">
+                    <p className="text-[#192160] text-sm font-medium mb-1 mt-2">
+                      Informe a nova descrição da sala
+                    </p>
+                    <textarea
+                      className="w-full px-2 py-1 rounded-[10px] border border-[#646999] text-[#777DAA] focus:outline-none text-xs font-medium"
+                      placeholder="Descrição do detalhamento sobre a sala"
+                      value={descricao}
+                      onChange={(e) => setDescricao(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="flex justify-center items-center mt-[10px] w-full">
+                    <button
+                      type="submit"
+                      className="px-3 py-2 border-[3px] rounded-xl font-semibold  text-sm flex gap-[4px] justify-center items-center  bg-[#16C34D] text-[#FFF]"
+                    >
+                      SALVAR ALTERAÇÕES
+                    </button>
+                  </div>
+                </form>
+              </div>
+            )}
+
+            {/* Fim adicionando pop up de editar sala */}
+           
           <button onClick={removeSala} className="flex gap-1 justify-start items-center font-medium text-sm text-rose-600 underline">
           <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="#e11d48" className="bi bi-x-lg" viewBox="0 0 16 16">
             <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
@@ -243,7 +328,7 @@ function voltarPagina(){
               <thead>
                 <tr>
                   <th className="text-left text-[10px] font-medium text-sky-900 min-w-1/4 max-w-24  ">Nome da sala</th>
-                  <th className="text-left text-[10px] font-medium text-sky-900 flex-1 mobile:w-[70%]">Descrição da sala</th>
+                  <th className="text-left text-[10px] font-medium text-sky-900 mobile:flex-1 mobile:w-[70%] w-[60%]">Descrição da sala</th>
                 </tr>
               </thead>
               <tbody>
@@ -251,7 +336,7 @@ function voltarPagina(){
                   <tr 
                     key={sala.id} 
                     className={`hover:bg-[#d5d8f1] cursor-pointer ${salaSelecionada === sala.id ? "bg-gray-200" : ""}`} 
-                    onClick={() => selecionaSala(sala.id)}
+                    onClick={() => statusSala(sala.id)}
                   >
                     <td className="px-2 py-2 text-xs text-[#646999] font-semibold border-2 border-solid border-[#B8BCE0] text-center">
                       {sala.nome}
@@ -267,7 +352,7 @@ function voltarPagina(){
             {/* fim tabela com todas as salas */}
 
             {/* passador de página */}
-            <div className=" mt-2 flex justify-end items-center">
+            <div className=" mt-2 flex justify-end items-center absolute bottom-3 right-8 mobile:right-10">
 
             <button onClick={voltarPagina} className="size-[22px] rounded-sm text-white text-sm flex items-center justify-center font-bold">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#075985" className="bi bi-chevron-left" viewBox="0 0 16 16">
@@ -275,7 +360,7 @@ function voltarPagina(){
           </svg>
             </button>
 
-            <div className="w-24 gap-1.5 px-2 py-1 flex items-center justify-center">
+            <div className="w-auto gap-1.5 px-1 py-1 flex items-center justify-center">
                 <div className="size-[28px] rounded-full bg-[#8d93c9] text-white text-sm flex items-center justify-center font-semibold">{paginaAtual}</div>
                 <div className="text-base text-sky-800 font-semibold">de <strong className="font-bold">{totalPaginas}</strong></div>
             </div>
@@ -295,8 +380,8 @@ function voltarPagina(){
         {/* fim conteudo central tela salas */}
 
         {/* logo chameco lateral */}
-          <div className="flex justify-start">
-            <img width={200} src="\public\logo_lateral.png" alt="logo chameco" />
+          <div className="flex absolute justify-start bottom-4">
+            <img className="mobile:w-[200px] w-32" src="\public\logo_lateral.png" alt="logo chameco" />
           </div>
         {/* fim logo chameco lateral */}
 
