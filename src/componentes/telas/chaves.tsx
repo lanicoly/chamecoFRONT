0b7e133143cbc0aa41664925cbd4b79f59060546
Priscila Freitas Martins <priscilafreitasmartins0@gmail.com>
2025-03-25 13:43:11 -0300

    adicionando usuarios e status

--- src/componentes/telas/chaves.tsx
+++ src/componentes/telas/chaves.tsx
@@ -47,8 +47,8 @@ export function Chaves() {
   const [isChavesModalOpen, setIsChavesModalOpen] = useState(false);
   //const [isDescricaoModalOpen, setIsDescricaoModalOpen] = useState(false);
 
-  // const [isPessoasModalOpen, setIsPessoasModalOpen] = useState(false);
-  // const [novaPessoa, setNovaPessoa] = useState("");
+  const [isUserModalOpen, setIsUserModalOpen] = useState(false);
+  const [novoUsuario, setNovoUsuario] = useState("");
   const [chaveSelecionada, setChaveSelecionada] = useState<Chaves | null>(null);
 
   //const blocos = ["Bloco C", "Bloco E", "Bloco J", "Bloco D"];
@@ -103,7 +103,7 @@ export function Chaves() {
   }, []);
   
   const API_URL = 'https://chamecoapi.pythonanywhere.com/chameco/api/v1/chaves/';
-  const token = "2838707856c2e91ed298b9172279d275c281e7d43cf2cd6f4c535dfd1170f6df";
+  const token = "9e12b2fc218bff944312056c564fe7e339e08ecbc10463d6c8655f958224bc20";
 
 //Funcao para a requisicao GET
 async function obterChaves(){
@@ -233,7 +233,7 @@ async function excluirChaveAPI(chaveId: number) {
     //setSelectedBloco("");
     //setDescricao("");
     closeChavesModal();
-    // closePessoasModal();
+    closeUserModal();
   }
   
   function statusSelecao(id: number) {
@@ -305,29 +305,29 @@ async function excluirChaveAPI(chaveId: number) {
       
   }
 }
-  // function adicionarPessoa(chaveId: number, novaPessoa: string) {
-  //   setItensAtuais((prevLista) => {
-  //     return prevLista.map((chave) => {
-  //       if (chave.id === chaveId) {
-  //         return {
-  //           ...chave,
-  //           pessoasAutorizadas: [...chave.pessoasAutorizadas, novaPessoa],
-  //         };
-  //       }
-  //       return chave;
-  //     });
-  //   });
-  // }
+   function adicionarUsuarios(chaveId: number, novoUsuario: string) {
+     setItensAtuais((prevLista) => {
+      return prevLista.map((chave) => {
+         if (chave.id === chaveId) {
+           return {
+             ...chave,
+             usuarios: [...chave.usuarios, novoUsuario],
+           };
+         }
+         return chave;
+       });
+     });
+   }
 
-  // function openPessoasModal(chave: Chaves) {
-  //   setChaveSelecionada(chave);
-  //   setIsPessoasModalOpen(true);
-  // }
+   function openUserModal(chave: Chaves) {
+     setChaveSelecionada(chave);
+     setIsUserModalOpen(true);
+   }
 
-  // function closePessoasModal() {
-  //   setIsPessoasModalOpen(false);
-  //   setChaveSelecionada(null);
-  // }
+   function closeUserModal() {
+     setIsUserModalOpen(false);
+     setChaveSelecionada(null);
+   }
 
   function closeChavesModal() {
     setSelectedSala(0);
@@ -360,7 +360,7 @@ async function excluirChaveAPI(chaveId: number) {
   }
 
   // adicionando função de excluir chave
-  function removeUser(e: React.FormEvent) {
+  function removeChave(e: React.FormEvent) {
     e.preventDefault();
   
     if (chaveSelecionada) {
@@ -459,7 +459,7 @@ async function excluirChaveAPI(chaveId: number) {
               {isDeleteModalOpen && (
                 <div className="fixed flex items-center justify-center inset-0 bg-black bg-opacity-50 z-20">
                   <form
-                    onSubmit={removeUser}
+                    onSubmit={removeChave}
                     className="container flex flex-col gap-2 w-full p-[10px] h-auto rounded-[15px] bg-white mx-5 max-w-[400px] justify-center items-center"
                   >
                     <div className="flex justify-center mx-auto w-full max-w-[90%]">
@@ -502,7 +502,7 @@ async function excluirChaveAPI(chaveId: number) {
                   </form>
                 </div>
               )}
-              {/* Fim adicionando pop up de deletar usuario */}
+              {/* Fim adicionando pop up de deletar cahve */}
 
               <BotaoAdicionar text = "ADICIONAR CHAVE" onClick={openChavesModal}/>
 
@@ -649,9 +649,9 @@ async function excluirChaveAPI(chaveId: number) {
                   <th className="text-left text-[10px] sm:text-[12px] font-medium text-sky-900 flex-1 w-[20%]">
                     Quantidade de chaves
                   </th>
-                  {/* <th className="text-left text-[10px] sm:text-[12px] font-medium text-sky-900 flex-1 w-[30%]">
+                  <th className="text-left text-[10px] sm:text-[12px] font-medium text-sky-900 flex-1 w-[30%]">
                     Lista de pessoas autorizadas
-                  </th> */}
+                  </th> 
                 </tr>
               </thead>
               <tbody>
@@ -714,7 +714,28 @@ async function excluirChaveAPI(chaveId: number) {
                         </p>
                       </div>
                     </td>
-                    
+                    <button  className="border-2 border-[#B8BCE0] border-solid bg-[#081683] ">
+                     <td className="align-center p-0.5 font-semibold  w-[40%] tablet:max-w-[200px] laptop:max-w-[400px] break-words ">
+                        <div className=" flex justify-center items-center mr-1">
+                          <svg className="size-6 ml-2 mr-2"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 35" fill="none">
+                            <g clip-path="url(#clip0_1781_438)">
+                              <path d="M18 14.5833C17.1347 14.5833 16.2888 14.3267 15.5694 13.846C14.8499 13.3653 14.2892 12.682 13.958 11.8826C13.6269 11.0831 13.5403 10.2035 13.7091 9.35481C13.8779 8.50615 14.2946 7.7266 14.9064 7.11474C15.5183 6.50289 16.2978 6.08621 17.1465 5.9174C17.9951 5.74859 18.8748 5.83523 19.6742 6.16636C20.4737 6.49749 21.1569 7.05825 21.6377 7.77771C22.1184 8.49718 22.375 9.34304 22.375 10.2083C22.375 11.3687 21.9141 12.4815 21.0936 13.3019C20.2731 14.1224 19.1603 14.5833 18 14.5833ZM25.2917 20.4167C25.2917 19.2563 24.8307 18.1435 24.0103 17.3231C23.1898 16.5026 22.077 16.0417 20.9167 16.0417H15.0833C13.923 16.0417 12.8102 16.5026 11.9897 17.3231C11.1693 18.1435 10.7083 19.2563 10.7083 20.4167V23.3333H13.625V20.4167C13.625 20.0299 13.7786 19.659 14.0521 19.3855C14.3256 19.112 14.6966 18.9583 15.0833 18.9583H20.9167C21.3034 18.9583 21.6744 19.112 21.9479 19.3855C22.2214 19.659 22.375 20.0299 22.375 20.4167V23.3333H25.2917V20.4167ZM18.0131 34.5115C17.2937 34.5119 16.5992 34.2477 16.0619 33.7692L10.596 29.1667H0.5V4.375C0.5 3.21468 0.960936 2.10188 1.78141 1.28141C2.60188 0.460936 3.71468 0 4.875 0L31.125 0C32.2853 0 33.3981 0.460936 34.2186 1.28141C35.0391 2.10188 35.5 3.21468 35.5 4.375V29.1667H25.506L19.8958 33.8042C19.3761 34.2626 18.7061 34.5142 18.0131 34.5115ZM3.41667 26.25H11.6621L17.9694 31.5656L24.459 26.25H32.5833V4.375C32.5833 3.98823 32.4297 3.61729 32.1562 3.3438C31.8827 3.07031 31.5118 2.91667 31.125 2.91667H4.875C4.48823 2.91667 4.11729 3.07031 3.8438 3.3438C3.57031 3.61729 3.41667 3.98823 3.41667 4.375V26.25Z" fill="white"/>
+                            </g>
+                                <defs>
+                                  <clipPath id="clip0_1781_438">
+                                    <rect width="35" height="35" fill="white" transform="translate(0.5)"/>
+                                  </clipPath>
+                                </defs>
+                          </svg>
+                            <p className=" text-xs text-[#FFFF] text-center  text-[15px] font-semibold leading-normal truncate">
+                            Pessoas autorizadas
+                            </p>
+                        </div>
+                      </td>
+                    </button>
+                    <td className={` text-center p-2 text-sm text-white font-semibold border-2 border-solid border-[#B8BCE0]  break-words min-w-8 w-20 ${chaves.disponivel === true ? 'bg-[#22b350]' : 'bg-red-700'}`}>
+                    {chaves.disponivel}
+                    </td>
 
                     <td className="align-center p-0.5 tablet:max-w-[200px] laptop:max-w-[400px] break-words">
                       <div className="flex w-[96px] h-[20px] pr-[2px] justify-center items-start gap-[2px] flex-shrink-0">
